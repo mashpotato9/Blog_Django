3657a0374327da36bb54295e193aa5372dd781ca
mashpotato9 <allenjia98@gmail.com>
2025-07-17 14:39:47 -0400

    implemented post blog action for backend

---
+++ blog/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class BlogForm(forms.Form):
+    title = forms.CharField(max_length=200, min_length=2, required=True)
+    content = forms.CharField(min_length=2, required=True)
+    category = forms.IntegerField(required=True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from .models import Category, Blog, Comment
+from .forms import BlogForm
 
 # Create your views here.
 def index(request):
@@ -10,7 +14,28 @@ def detail(request, post_id):
     return render(request, 'details.html')
 
 @login_required(login_url='blogAuth:login')
+@require_http_methods(["GET", "POST"])
 def post_blog(request):
     # Logic to handle posting a blog
-    return render(request, 'post_blog.html')
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        return render(request, 'post_blog.html', context={'categories': categories})
 
+    elif request.method == 'POST':
+        # Handle form submission
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            category_id = form.cleaned_data['category']
+            # Logic to save the blog post
+            Blog.objects.create(
+                title=title,
+                content=content,
+                category_id=category_id,
+                author=request.user
+            )
+            return JsonResponse({'status': 201, 'message': 'Blog posted successfully'})
+        else:
+            print(form.errors)
+            return JsonResponse({'status': 400, 'errors': form.errors})

5f47151b4d6d8f4685d6a0f992d14c82dc1b85e3
mashpotato9 <allenjia98@gmail.com>
2025-07-17 14:04:44 -0400

    set up default admin portal

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Blog, Comment
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at', 'content')
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('blog', 'author', 'created_at', 'content')
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
+    
--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ User = get_user_model()
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
@@ -13,9 +16,15 @@ class Blog(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content
